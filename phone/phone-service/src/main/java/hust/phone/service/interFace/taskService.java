beda726eaa9986d735aebd1a04f23a3e50dd64c4
hp <hp@DESKTOP-VE7T811>
2018-07-02 20:21:49 +0800

    第一次提交

---
+++ phone/phone-service/src/main/java/hust/phone/service/interFace/taskService.java
@@ -0,0 +1,10 @@
+package hust.phone.service.interFace;
+
+import java.util.List;
+
+import hust.phone.mapper.pojo.Task;
+
+public interface taskService {
+	public List<Task> getAllTask(Task task);
+
+}
