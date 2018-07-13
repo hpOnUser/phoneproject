f6252543ef6253315da9860f849bd09e86f7f7e6
hp <hp@windows10.microdone.cn>
2018-07-13 16:39:57 +0800

    修改拦截器，添加欢迎界面

--- phoneproject/phone/phone-web/src/main/java/hust/phone/web/controller/indexController.java
+++ phoneproject/phone/phone-web/src/main/java/hust/phone/web/controller/indexController.java
@@ -1,6 +1,7 @@
 package hust.phone.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +18,12 @@ public class indexController {
 	{
 		return "fight";
 	}
+	
+	@RequestMapping("/webtest")
+	public String webtest(Model model)
+	{
+		return "webtest";
+	}
+
 
 }
