22436c8bd410cdb4fc2be4a368982f40a669dee9
hujunhui123 <hujunhui123@outlook.com>
2018-07-16 10:04:39 +0800

    修改无人机操纵及工单页面

---
+++ phoneproject/phone/phone-dao/src/main/java/hust/phone/mapper/pojo/Exetask.java
@@ -0,0 +1,38 @@
+package hust.phone.mapper.pojo;
+
+public class Exetask {
+
+	private String exeid;
+	
+	private String taskid;
+	
+	private String status;
+	
+	//飞机的位置
+	private String flongda;
+
+	public String getExeid() {
+		return exeid;
+	}
+
+	public void setExeid(String exeid) {
+		this.exeid = exeid;
+	}
+
+	public String getTaskid() {
+		return taskid;
+	}
+
+	public void setTaskid(String taskid) {
+		this.taskid = taskid;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+}
--- phoneproject/phone/phone-web/src/main/java/hust/phone/web/controller/TaskController.java
+++ phoneproject/phone/phone-web/src/main/java/hust/phone/web/controller/TaskController.java
@@ -3,18 +3,23 @@ package hust.phone.web.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.omg.CORBA.Request;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import hust.phone.mapper.pojo.Task;
 import hust.phone.mapper.pojo.User;
 import hust.phone.service.interFace.UserService;
 import hust.phone.service.interFace.taskService;
+import hust.phone.utils.pojo.JsonView;
+import hust.phone.utils.pojo.PhoneUtils;
 
 @Controller
 public class TaskController {
@@ -29,7 +34,21 @@ public class TaskController {
 	public String toTaskList() {
 		return "task";
 	}
-
+	
+	//确认任务
+	//如果用户角色是放飞者，那么修改该任务的状态为 2
+	//如果用户是接收者，那么修改该任务的状态为 3，同时在exe表中插入一条新的数据
+	@RequestMapping(value="/ensureTask",method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+	@ResponseBody
+	public String ensureTask(Task task,HttpServletRequest request) {
+		
+		User user = PhoneUtils.getLoginUser(request);
+			
+		
+		String reString = "已经确认任务";
+		return JsonView.render(1, reString);
+	}
+	
 	// 查询所有的工作单
 	@RequestMapping("/taskList")
 	public String taskList(String flag, HttpSession session, Model model) {
@@ -41,8 +60,7 @@ public class TaskController {
 		User user = userServiceImpl.getUserById(userId);
 		// 得到用户角色
 		String role = user.getRole();
-		List<Task> taskList = new ArrayList<Task>();
-		
+		List<Task> taskList = new ArrayList<Task>();	
 		//判断是哪类查询条件
 		switch (flag) {
 		case "0":
@@ -55,33 +73,22 @@ public class TaskController {
 			task.setFinishstatus(null);
 			break;
 		}
-		// 根据用户角色，判断相应任务的用户
-		if ("1".equals(role)) {
-
-			task.setUserbid(user.getUserid());
-			taskList = taskServiceImpl.getAllTask(task);
-			model.addAttribute("taskList", taskList);
-			return "C1subtasklist";
-
-		} else {
-
-			task.setUsercid(user.getUserid());
-			taskList = taskServiceImpl.getAllTask(task);
-			model.addAttribute("taskList", taskList);
-			return "C2subtasklist";
-		}
-
+		
+		task.setUserbid(user.getUserid());
+		taskList = taskServiceImpl.getAllTask(task);
+		model.addAttribute("taskList", taskList);
+		
+		return "subtasklist";
 	}
 	
+	private int Number = -2;
+	//轮询新的工单数目
 	@RequestMapping("getTaskNumber")
 	@ResponseBody
-	public User getTaskNumber(String userid){
-		
-		
-		
-		
-		
-		return null;
+	public String getTaskNumber(){				
+	    //在这里查询最新的工单数目
+		Number = Number + 1;
+		return Number+"";
 	}
 
 }
